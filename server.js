99faec006db8a0704fcdf82f5f1b387d51dba993
Will Rowston <willrowston@gmail.com>
2020-05-21 10:52:37 -0400

    added react app

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json('hello world')
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(`${__dirname}/client/build`))
+
+// app.get('/', (req, res) => {
+//     res.json('hello world')
+// })
 
 
 const PORT = process.env.PORT || 3001
