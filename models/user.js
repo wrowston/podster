63449ed6bfbbb61ae776c34c699fe5b5a2d8ec90
Will Rowston <willrowston@gmail.com>
2020-05-21 11:34:39 -0400

    added user model

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('./connection.js')
+
+const UserSchema = new mongoose.Schema({
+    userName: String,
+    email: String,
+    following: Array,
+    favoritedEpisodes: Array,
+    currentlyListening: String,
+})
+
+const UserModel = mongoose.Model('user', UserSchema)
+
+
+function getAllUsers() {
+    return UserModel.find({})
+}
+
+function getUserById(userId) {
+    return UserModel.findById(userId)
+}
+
+function createUser(userData) {
+    return UserModel.create(userData)
+}
+
+function updateUser(userId, userData) {
+    UserModel.findByIdAndUpdate(userId, userData)
+}
+
+function deleteUser(userId) {
+    return UserModel.findByIdAndDelete(userId)
+}
+
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+}
