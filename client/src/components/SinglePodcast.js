82129bb32dea47ddd68cefafb08a827e52f105d4
Will Rowston <willrowston@gmail.com>
2020-05-21 15:49:48 -0400

    displayed single podcast

--- client/src/components/SinglePodcast.js
+++ client/src/components/SinglePodcast.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class SinglePodcast extends Component {
+
+    state = {
+        podcast: {
+            name: '',
+            creator: '',
+            description: '',
+            genre: '',
+            rating: '',
+            episodes: [],
+            followers: 0,
+            image: ''
+        }
+    }
+
+    componentDidMount() {
+        this.getPodcastById()
+    }
+
+    getPodcastById = async () => {
+        const podcastId = this.props.match.params.podcastId
+        console.log('podcastId', podcastId)
+        const res = await axios.get(`/api/podcast/${podcastId}`)
+        this.setState(res.data)
+    }
 
 
     render() {
         return (
             <div>
                 <h3>Single Podcast</h3>
+                <div>
+                    <img src={this.state.image} />
+                    <h1>{this.state.name}</h1>
+                    <h3>{this.state.creator}</h3>
+                    <div>{this.state.description}</div>
+                    <div>{this.state.genre}</div>
+                    <div>{this.state.rating}</div>
+                    <div>{this.state.followers}</div>
+                </div>
             </div>
         )
     }
