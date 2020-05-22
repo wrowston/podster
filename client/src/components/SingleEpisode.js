a85a757c909185f3b3f45d835ca838fd5f6cae18
Will Rowston <willrowston@gmail.com>
2020-05-22 11:17:46 -0400

    added edit episode form

--- client/src/components/SingleEpisode.js
+++ client/src/components/SingleEpisode.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class SingleEpisode extends Component {
 
@@ -11,8 +12,9 @@ export default class SingleEpisode extends Component {
             length: '',
             favorites: 0,
             listens: 0,
-            podcastId: ''
+            episodeId: ''
         },
+        showEditForm: false
     }
 
     componentDidMount() {
@@ -27,17 +29,84 @@ export default class SingleEpisode extends Component {
         this.setState(newState)
     }
 
+
+    onDeleteEpisode = async (episodeId) => {
+        await axios.delete(`/api/episode/${episodeId}`)
+    }
+
+    toggleEditForm = () => {
+        const showEditForm = !this.state.showEditForm
+        this.setState({ showEditForm })
+    }
+
+    onChangeCurrentEpisode = (evt) => {
+        const newState = { ...this.state }
+        newState.episode[evt.target.name] = evt.target.value
+        this.setState(newState)
+    }
+
+    onSubmit = async (evt) => {
+        evt.preventDefault()
+        try {
+            const episodeId = this.props.match.params.episodeId
+            await axios.put(`/api/episode/${episodeId}`, this.state.episode)
+            this.getEpisodeById()
+        } catch (error) {
+            console.log('Failed to get episode')
+            console.log(error)
+        }
+    }
+
     render() {
         return (
             <div>
-                <div>
-                    <h2>{this.state.episode.name}</h2>
-                    <div>{this.state.episode.description}</div>
-                    <div>{this.state.episode.dateUploaded}</div>
-                    <div>{this.state.episode.length}</div>
-                    <div>Favorites: {this.state.episode.favorites}</div>
-                    <div>Listen: {this.state.episode.listens}</div>
-                </div>
+
+                {this.state.showEditForm
+                    ?
+                    <div>
+                        <h4>Edit Episode</h4>
+                        <form onSubmit={this.onSubmit}>
+                            <div>
+                                <label htmlFor="name">Name</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={this.state.episode.name}
+                                    onChange={this.onChangeCurrentEpisode}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="description">Description</label>
+                                <input
+                                    type="text"
+                                    name="description"
+                                    value={this.state.episode.description}
+                                    onChange={this.onChangeCurrentEpisode}
+                                />
+                            </div>
+                            <Link to={'/'}>
+                                <button onClick={() => this.onDeleteEpisode(this.props.match.params.episodeId)}>
+                                    Delete
+                        </button>
+                            </Link>
+                            <input type="submit" value="Save" />
+                        </form>
+                    </div>
+                    :
+                    <div>
+                        <h2>{this.state.episode.name}</h2>
+                        <div>{this.state.episode.description}</div>
+                        <div>{this.state.episode.dateUploaded}</div>
+                        <div>{this.state.episode.length}</div>
+                        <div>Favorites: {this.state.episode.favorites}</div>
+                        <div>Listen: {this.state.episode.listens}</div>
+                    </div>}
+
+                <button onClick={this.toggleEditForm}>
+                    {this.state.showEditForm
+                        ? 'Hide Edit Episode Form'
+                        : 'Edit Episode'}
+                </button>
             </div>
         )
     }
--- client/src/components/SinglePodcast.js
+++ client/src/components/SinglePodcast.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import AllEpisodes from './AllEpisodes'
+import AllEpisodes from './AllEpisodes.js'
 
 export default class SinglePodcast extends Component {
 
